b5839cf012e2838da6893d5cec9b3ecd5d0d3376
pyphilia <pyphilia@gmail.com>
2022-07-28 16:56:27 +0200

    feat: enable german

--- src/langs/index.ts
+++ src/langs/index.ts
@@ -1,9 +1,12 @@
 import messagesEN from './en/messages.json';
 import messagesFR from './fr/messages.json';
+import messagesDE from './de/messages.json';
 import chatboxEN from './en/chatbox.json';
 import chatboxFR from './fr/chatbox.json';
+import chatboxDE from './de/chatbox.json';
 import authEN from './en/auth.json';
 import authFR from './fr/auth.json';
+import authDE from './de/auth.json';
 
 // define namespaces
 export const namespaces = {
@@ -24,6 +27,12 @@ const fr = {
   [namespaces.messages]: messagesFR,
 };
 
+const de = {
+  [namespaces.auth]: authDE,
+  [namespaces.chatbox]: chatboxDE,
+  [namespaces.messages]: messagesDE,
+};
+
 // other supported languages here
 
-export { en, fr };
+export { en, fr, de };
