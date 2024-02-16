5917795e7f423f8c3c0b251297419f50e379f7db
Basile Spaenlehauer <spaenleh@gmail.com>
2024-02-16 14:51:06 +0100

    fix: test esm package (#358)

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,6 @@
 import i18n from 'i18next';
 
-import { ar, de, en, fr, it, namespaces } from './langs';
+import { ar, de, en, fr, it, namespaces } from './langs/index.js';
 
 export const DEFAULT_LANG = 'en';
 
