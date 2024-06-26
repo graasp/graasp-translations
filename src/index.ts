8dcf862c210d8c17a9f79104363f886fd27d468d
Basile Spaenlehauer <spaenleh@gmail.com>
2024-06-26 15:03:46 +0200

    fix: allow default export to be used (#449)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 export { namespaces } from './langs/index.js';
-export { buildI18n, langs, DEFAULT_LANG } from './i18n.js';
-
+export {
+  buildI18n,
+  buildI18n as default,
+  langs,
+  DEFAULT_LANG,
+} from './i18n.js';
 export * from './constants/association.js';
 export * from './constants/categories.js';
 export * from './constants/common.js';
